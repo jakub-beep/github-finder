7ffbc6fa026c51cd9e1e5ae2d2bd42c2f2ddf54e
Wytykowski <jakub.wytykowski@capgemini.com>
2022-12-22 21:48:47 +0100

    add link about to not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaBookOpen } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 function NotFound() {
@@ -8,10 +8,16 @@ function NotFound() {
         <div className="max-w-lg">
           <h1 className="text-8xl font-bold mb-8">Ooops!</h1>
           <p className="text-5xl mb-8">400 - Page not found!</p>
-          <Link to="/" className="btn btn-secondary btn-lg">
-            <FaHome className="mr-2" />
-            Back To Home
-          </Link>
+          <div className="flex justify-between">
+            <Link to="/" className="btn btn-secondary btn-lg">
+              <FaHome className="mr-2" />
+              Back To Home
+            </Link>
+            <Link to="/about" className="btn btn-primary btn-lg">
+              <FaBookOpen className="mr-2" />
+              Go To About
+            </Link>
+          </div>
         </div>
       </div>
     </div>
